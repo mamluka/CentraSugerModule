9a8150b992613586acf7edc6a1c1b7af51b90427
davidmz <david.mazvovsky@gmail.com>
2013-03-09 16:05:40 +0200

    small fix

--- lib/hooks/update-sold-services.php
+++ lib/hooks/update-sold-services.php
@@ -25,7 +25,7 @@ function UpdateSoldServicesDates(&$bean)
         $contracts = json_decode(file_get_contents(__DIR__ . "/echosign.json"), true);
         $contract_id = $contracts[$bean->googlelocal_contract_type_c];
 
-        $logger->LogInfo("The selected contract for: " . $$bean->googlelocal_contract_type_c . "was: " . $contract_id);
+        $logger->LogInfo("The selected contract for: " . $bean->googlelocal_contract_type_c . "was: " . $contract_id);
 
         $result = $soaClient->get("/echosign/send", array(
             'email' => $email,
