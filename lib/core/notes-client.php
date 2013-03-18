18fe77eb068882b6f4c2e05fb43a782406e37c18
davidmz <david.mazvovsky@gmail.com>
2013-03-18 22:53:44 +0200

    logging for notes

--- lib/core/notes-client.php
+++ lib/core/notes-client.php
@@ -7,10 +7,14 @@ class NotesClient
         $soaFactory = new SoaRestClient();
         $client = $soaFactory->Get();
 
+        $logger = new KLogger ("centra-logs", KLogger::DEBUG);
+
         $client->post('/crm/note', array(
             'id' => $id,
             'message' => $message
         ));
+
+        $logger->LogInfo("Note " . $message . " sent");
     }
 }
 
