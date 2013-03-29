26bfc251563af598a8ba1445fa4c0f7589c68726
davidmz <david.mazvovsky@gmail.com>
2013-03-29 18:26:18 +0300

    fix

--- lib/core/core.php
+++ lib/core/core.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once(__DIR__ . '/../core/klogger.php');
-require_once(__DIR__ . '/../core/email-rest-client.php');
 require_once(__DIR__ . '/../core/soa-rest-client.php');
 require_once(__DIR__ . '/../core/crm-utils.php');
 require_once(__DIR__ . '/../core/notes-client.php');
